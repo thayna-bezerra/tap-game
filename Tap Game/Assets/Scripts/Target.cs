6c0e1c129b9ba0f553acddd627a4bb89e5890410
thayna-bezerra <arraisthayna2106@gmail.com>
2024-02-13 12:46:17 -0300

    programming position and speed of game items

--- Tap Game/Assets/Scripts/Target.cs
+++ Tap Game/Assets/Scripts/Target.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //velocidade dos objetos
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f; //rotação dos objetos
+
+    //local que o objeto será spawnado
+    private float xRange = 5f;
+    private float ySpawnPos = -6f;
+
+    //corpo do objeto
+    private Rigidbody rb;
+
     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.position = RandomSpawnPos();
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 RandomForce() 
     {
-        
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque() 
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
