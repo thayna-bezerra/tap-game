f80eda66759042183b28a718eabf7f8594b59cdb
thayna-bezerra <arraisthayna2106@gmail.com>
2024-02-16 23:05:03 -0300

    implementing ServiceAPI class

---
+++ Tap Game/Assets/Scripts/ServiceAPI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class ServiceAPI
+{
+    //endereco da api
+    const string URL = "http://tapgameapp.reactivit.com.br/";
+
+    //objeto para interagir com a api
+    public static HttpClient GetClient()
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        client.DefaultRequestHeaders.Add("Connection", "close");
+        return client;
+    } 
+
+    public static async Task<User> GetUser(string nick)
+    {
+        HttpClient client = GetClient();
+        var responsive = await client.GetAsync(URL + "user/" + nick);
+
+        //pegou os dados da api?
+        if(responsive.IsSuccessStatusCode == true)
+        {
+            string content = await responsive.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<User>(content);
+        }
+        return new User();
+    }
+}
