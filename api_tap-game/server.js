6f619ccf0e60947f299a24af5da592cef621eeb9
thayna-bezerra <arraisthayna2106@gmail.com>
2024-03-09 19:25:47 -0300

    add library dotenv

--- api_tap-game/server.js
+++ api_tap-game/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
+require('dotenv').config() //import and configurate library
+const express = require('express'); //import the framework express
 
-const server = express();
+const server = express(); //initialize express
 
+//function for read route
 server.use("/ping", (req, res) => {{
     res.send("pong");
 }});
 
-server.listen(3001, () => {
-    console.log("Server running in port 3001");
+//server listening requisitions
+server.listen(process.env.PORT, () => {
+    console.log("Server running in port address: " + process.env.BASE);
 });
