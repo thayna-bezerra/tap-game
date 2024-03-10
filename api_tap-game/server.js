b1cb11ba8ea5138cd55ae1ed6eaf15e8d398eab2
thayna-bezerra <arraisthayna2106@gmail.com>
2024-03-10 00:01:41 -0300

    separating the routes

--- api_tap-game/server.js
+++ api_tap-game/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config() //import and configurate library
 const express = require('express'); //import the framework express
-
+const ApiRoute = require('./src/routes/routes')
 const server = express(); //initialize express
 
+server.use(express.json());
+
+server.use(express.urlencoded({extended:true}))
+
 //function for read route
-server.use("/ping", (req, res) => {{
-    res.send("pong");
-}});
+server.use("/", ApiRoute);
 
 //server listening requisitions
 server.listen(process.env.PORT, () => {
